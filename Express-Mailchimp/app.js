49e4bde850f641d8f1e721ee4ef51ffbfa80f4d1
Ankit Gupta <58206597+devankit01@users.noreply.github.com>
2021-05-21 20:12:23 +0530

    Add files via upload

--- Express-Mailchimp/app.js
+++ Express-Mailchimp/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const Mailchimp = require("mailchimp-api-v3");
+const mailchimp = new Mailchimp("840cea6730713c2e6d8bab4bb2849489-us6"); // API-Key
+
 
 
 const app = express();
@@ -12,44 +15,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
+  mailchimp
+    .post("/lists/71aec5dd6a/members", { // Include Your id
+      email_address: req.body.email,
+      merge_fields: {
+        FNAME: req.body.firstName,
+        LNAME: req.body.lastName,
+      },
 
-  var data = {
-    members: [
-      {
-        email_address: email,
-        status: "Subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        }
-      }
-    ]
-  };
-
-  var jsondata = JSON.stringify(data);
-
-
-  url = "https://us6.api.mailchimp.com/3.0/lists/?id=71aec5dd6a/members/";
-
-  const options = {
-    method: "POST",
-    auth: "devankit:840cea6730713c2e6d8bab4bb2849489-us6",
-  };
-
-  const request = https.request(url, options, (response) => {
-    response.on("data", (data) => {
-      console.log(JSON.parse(data));
+      status: "subscribed",
+    })
+    .then(function (results) {
+      console.log(results);
+    })
+    .catch(function (err) {
+      console.log(err);
     });
-  });
 
-  request.write(jsondata);
-  request.end();
+  res.sendFile(__dirname + "/success.html");
 });
 
 app.listen(8080, (req, res) => console.log("Server Started"));
 
-// id = ''
-// api = ''
