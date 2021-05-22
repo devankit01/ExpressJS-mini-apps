21b9af9b6d841952ee93817bdd69bdeb1c9bc0c4
Ankit Gupta <58206597+devankit01@users.noreply.github.com>
2021-05-23 00:04:03 +0530

    added

--- ExpressJS-Blog-App-V.0.1/app.js
+++ ExpressJS-Blog-App-V.0.1/app.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const PORT = process.env.PORT || 3000 ;
 
+// Initialise app
+const app = express();
+
+//  bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// View engine Setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Static
+app.use(express.static("public"));
+app.use(express.json());
+
+
+// Routes
+app.get('/', (req,res) => {
+    res.render('Home')
+})
+
+app.listen(PORT , (req, res) => console.log('Server Started'))
